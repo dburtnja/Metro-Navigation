42f6ac68c44491afc18148577385b4b84838f5e9
dburtnja <dostup.te@gmail.com>
2017-05-14 23:12:21 +0300

    add metro

---
+++ MetroNavigation/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetroNavigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point start;
+        private Point end;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void animation(PathFigure pFigure, int animationTime)
+        {
+            // Create a NameScope for the page so that
+            // we can use Storyboards.
+            NameScope.SetNameScope(this, new NameScope());
+
+            // Create a rectangle.
+            Rectangle aRectangle = new Rectangle();
+            aRectangle.Width = 30;
+            aRectangle.Height = 30;
+            aRectangle.Fill = new ImageBrush(new BitmapImage(new Uri(@"image/train.png", UriKind.Relative)));
+
+            // Create a transform. This transform
+            // will be used to move the rectangle.
+            TranslateTransform animatedTranslateTransform =
+                new TranslateTransform();
+
+            // Register the transform's name with the page
+            // so that they it be targeted by a Storyboard.
+            this.RegisterName("AnimatedTranslateTransform", animatedTranslateTransform);
+
+            aRectangle.RenderTransform = animatedTranslateTransform;
+
+            // Create a Canvas to contain the rectangle
+            // and add it to the page.
+            Canvas mainPanel = MainGrid;
+            mainPanel.Children.Add(aRectangle);
+            this.Content = mainPanel;
+
+            // Create the animation path.
+            PathGeometry animationPath = new PathGeometry();
+            animationPath.Figures.Add(pFigure);
+
+            // Freeze the PathGeometry for performance benefits.
+            animationPath.Freeze();
+
+            // Create a DoubleAnimationUsingPath to move the
+            // rectangle horizontally along the path by animating 
+            // its TranslateTransform.
+            DoubleAnimationUsingPath translateXAnimation =
+                new DoubleAnimationUsingPath();
+            translateXAnimation.PathGeometry = animationPath;
+            translateXAnimation.Duration = TimeSpan.FromSeconds(animationTime);
+
+            // Set the Source property to X. This makes
+            // the animation generate horizontal offset values from
+            // the path information. 
+            translateXAnimation.Source = PathAnimationSource.X;
+
+            // Set the animation to target the X property
+            // of the TranslateTransform named "AnimatedTranslateTransform".
+            Storyboard.SetTargetName(translateXAnimation, "AnimatedTranslateTransform");
+            Storyboard.SetTargetProperty(translateXAnimation,
+                new PropertyPath(TranslateTransform.XProperty));
+
+            // Create a DoubleAnimationUsingPath to move the
+            // rectangle vertically along the path by animating 
+            // its TranslateTransform.
+            DoubleAnimationUsingPath translateYAnimation =
+                new DoubleAnimationUsingPath();
+            translateYAnimation.PathGeometry = animationPath;
+            translateYAnimation.Duration = TimeSpan.FromSeconds(animationTime);
+
+            // Set the Source property to Y. This makes
+            // the animation generate vertical offset values from
+            // the path information. 
+            translateYAnimation.Source = PathAnimationSource.Y;
+
+            // Set the animation to target the Y property
+            // of the TranslateTransform named "AnimatedTranslateTransform".
+            Storyboard.SetTargetName(translateYAnimation, "AnimatedTranslateTransform");
+            Storyboard.SetTargetProperty(translateYAnimation,
+                new PropertyPath(TranslateTransform.YProperty));
+
+            // Create a Storyboard to contain and apply the animations.
+            Storyboard pathAnimationStoryboard = new Storyboard();
+        //    pathAnimationStoryboard.RepeatBehavior = RepeatBehavior.ReferenceEquals;
+            pathAnimationStoryboard.Children.Add(translateXAnimation);
+            pathAnimationStoryboard.Children.Add(translateYAnimation);
+
+            // Start the animations when the rectangle is loaded.
+            aRectangle.Loaded += delegate (object sender, RoutedEventArgs e)
+            {
+                // Start the storyboard.
+                pathAnimationStoryboard.Begin(this);
+            };
+        }
+
+        private void MouseLeftButtonDownOnGrid(object sender, MouseButtonEventArgs e)
+        {
+            if (start.Equals(new Point()))
+                start = Mouse.GetPosition(MainGrid);
+            else
+            {
+                end = Mouse.GetPosition(MainGrid);
+                Metro metro = new Metro();
+                PathFigure pFigure = metro.findPath(start, end);
+                MessageBox.Show("Mouse on: x = " + start.X + " y = " + start.Y);
+                if (pFigure == null)
+                    MessageBox.Show("Помилка пошуку шляху");
+                else
+                    animation(pFigure, pFigure.Segments.Count);
+            }
+        }
+
+        private void MouseRightButtonDownOnGrid(object sender, MouseButtonEventArgs e)
+        {
+            Point p = Mouse.GetPosition(MainGrid);
+            MessageBox.Show("Mouse on: x = " + p.X + " y = " + p.Y);
+            start = new Point();
+            end = new Point();
+        }
+    }
+}
---
+++ MetroNavigation/Metro.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MetroNavigation
+{
+    class Metro
+    {
+        public int linesNbr;
+        public Line[] lines;
+
+        public Metro()
+        {
+            //Reading map from file
+            StreamReader streamReader = new StreamReader("Map/Map.txt", Encoding.GetEncoding(1251));
+
+            //Reading number of MetroLines
+            linesNbr = Convert.ToInt16(Regex.Replace(streamReader.ReadLine(), "LineNumder=", String.Empty));
+
+            lines = new Line[linesNbr];
+            //Reading all MetroLines
+            for (int i = 0; i < linesNbr; i++)
+            {
+                lines[i] = new Line(streamReader, i + 1);
+            }
+        }
+
+        public PathFigure findPath(Point start, Point end)
+        {
+            Station startStation = null;
+            Station endStation = null;
+
+            //find start station
+            for (int i = 0; i < linesNbr && startStation == null; i++)
+            {
+                startStation = lines[i].findStation(start);
+            }
+
+            //find end station
+            for (int i = 0; i < linesNbr && endStation == null; i++)
+            {
+                endStation = lines[i].findStation(end);
+            }
+
+            if (startStation == null || endStation == null)
+                return null;
+
+            //create path
+            PathFigure pFigure = new PathFigure();
+            //set start point
+            pFigure.StartPoint = startStation.stationCoordinates;
+
+            if ((int)(startStation.stationNbr / 100) == (int)(endStation.stationNbr / 100))
+            {
+                PathSegmentCollection pathCollection = new PathSegmentCollection();
+
+                int line = (int)(startStation.stationNbr / 100) - 1;
+                lines[line].writeLineSegment(pathCollection, startStation, endStation);
+                if (pathCollection == null)
+                    return null;
+                pFigure.Segments = pathCollection;
+                return pFigure;
+            }
+
+            return pFigure;
+        }
+    }
+
+    class Line
+    {
+        public int lineNbr;
+        public int stationsNbr;
+        public Station[] stations;
+
+        public Line(StreamReader streamReader, int line)
+        {
+            lineNbr = line;
+            //Reading number of MetroLines
+            stationsNbr = Convert.ToInt16(Regex.Replace(streamReader.ReadLine(), "Line" + line + "Stations=", String.Empty));
+
+            stations = new Station[stationsNbr];
+            //Reading all stations
+            for (int i = 0; i < stationsNbr; i++)
+            {
+                stations[i] = new Station(streamReader.ReadLine(), i);
+            }
+        }
+
+        public Station findStation(Point p)
+        {
+            Station station = null;
+
+            for (int i = 0; i < stationsNbr && station == null; i++)
+            {
+                station = stations[i].isInRange(p);
+            }
+            return station;
+        }
+
+        public void writeLineSegment(PathSegmentCollection pathCollection, Station from, Station till)
+        {
+            int fromNbr = from.arrayNbr;
+
+            if (fromNbr < till.arrayNbr)
+            {
+                //adding all points
+                while (fromNbr <= till.arrayNbr)
+                {
+                    LineSegment lineSegment = new LineSegment();
+                    lineSegment.Point = stations[fromNbr].stationCoordinates;
+                    pathCollection.Add(lineSegment);
+                    fromNbr++;
+                }
+            }
+            else if (fromNbr > till.arrayNbr)
+            {
+                while (fromNbr >= till.arrayNbr)
+                {
+                    LineSegment lineSegment = new LineSegment();
+                    lineSegment.Point = stations[fromNbr].stationCoordinates;
+                    pathCollection.Add(lineSegment);
+                    fromNbr--;
+                }
+            }
+            else
+                pathCollection = null;
+        }
+    }
+
+    class Station
+    {
+        public int arrayNbr;
+        public int stationNbr;
+        public String stationName;
+        public Point stationCoordinates;
+        public int transferStation = 0;
+
+        public Station(String input, int i)
+        {
+            arrayNbr = i;
+            if (input == null)
+                MessageBox.Show("Помилка зчитування файлу карти");
+            else
+            {
+                string[] array = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                stationNbr = Convert.ToInt16(array[0]);
+                stationName = array[1];
+                stationCoordinates = new Point { X = Convert.ToInt16(array[2]), Y = Convert.ToInt16(array[3]) };
+                if (array.Length == 5)
+                    transferStation = Convert.ToInt16(array[4]);
+            }
+        }
+
+        public Station isInRange(Point p)
+        {
+            if (Point.Subtract(p, stationCoordinates).Length < 10)
+                return this;
+            return null;
+        }
+    }
+}
